23a0553579ca9a046a5add29069bc4a34bcc3584
22495521 <d22495521@gmail.com>
2023-01-09 10:38:05 +0800

    up

---
+++ js/login.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data(){
+       return{
+        userType:{
+            "username": "",
+            "password": "",
+          }
+       }
+    },
+    methods:{
+        login(){
+
+            const url='https://vue3-course-api.hexschool.io/v2';
+            axios.post(`${url}/admin/signin`, this.userType)
+                .then((res)=>{
+                    const {token,expired} = res.data
+                    //存入cookie
+                    document.cookie = `bigtoken=${token}; expired=${expired}`;
+                    //傳送至別的網頁
+                    
+                    //從cookie取得token//從cookie取得token
+                    document.location.href="products.html";
+
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    alert('帳號密碼錯誤');
+                })
+        }
+    },
+    mounted(){
+    }
+})
+
+
+app.mount('#app');
---
+++ js/products.js
@@ -0,0 +1,144 @@
+let productsDom = '';
+let deletepProductsDom='';
+
+
+const app = Vue.createApp({
+    data(){
+        return{
+            url:'https://vue3-course-api.hexschool.io/v2',
+            path:'biggo',
+            products:{
+            },
+            alertTitle:0,
+
+            alertProducts:{
+                "imagesUrl": [],
+            },
+        }
+    },
+    methods:{
+        getProducts(){
+
+            axios.get(`${this.url}/api/${this.path}/admin/products/all`)
+            .then((res)=>{
+                this.products = res.data.products;
+            })
+            .catch((error)=>{
+                //失敗傳送回login
+                console.log(error);
+            })
+        },
+
+        openProducts(){
+            this.alertProducts = {
+                "imagesUrl": [],
+            };
+            productsDom.show();
+        },
+        enterProducts(){
+
+            //這區是有新增跟編輯
+            if(this.alertProducts.id){
+                 //此為編輯
+                 axios.put(`${this.url}/api/${this.path}/admin/product/${this.alertProducts.id}`,{data:{...this.alertProducts}})
+                 .then((res)=>{
+                    productsDom.hide();
+                    //刷新頁面
+                    this.getProducts();
+                 })
+                 .catch((error)=>{
+                    alert('重新填寫');
+     
+                 }) 
+                 
+            }
+            else
+            {   //此為新增
+                axios.post(`${this.url}/api/${this.path}/admin/product`,{data:{...this.alertProducts}})
+                .then((res)=>{
+                    productsDom.hide();
+                    //刷新頁面
+                    this.getProducts();
+                })
+                .catch((error)=>{
+                    alert('重新填寫');
+                })  
+            }
+        },
+        addAlertImg(){
+            if(!this.alertProducts.imagesUrl){
+                this.alertProducts.imagesUrl = [];
+            }
+            this.alertProducts.imagesUrl.push('');
+        },
+        deleteAlertImg(){
+            this.alertProducts.imagesUrl.pop();
+        },
+
+
+        editData(id){
+            this.alertTitle = 0;
+
+            const data = Object.values(this.products);
+            data.forEach((i)=>{
+                if(i.id===id){
+                    this.alertProducts={
+                        ...i
+                    }
+                }
+            })
+            productsDom.show();
+        },
+
+        openDeleteDom(id){
+            const data = Object.values(this.products);
+            data.forEach((i)=>{
+                if(i.id===id){
+                    this.alertProducts={
+                        ...i
+                    }
+                }
+            })
+            delProductModal.show();
+        },
+
+        DeleteData(){
+            axios.delete(`${this.url}/api/${this.path}/admin/product/${this.alertProducts.id}`,{data:{...this.alertProducts}})
+                 .then((res)=>{
+                    delProductModal.hide();
+                    //刷新頁面
+                    this.alertProducts={
+                        "imagesUrl": [],
+                    };
+                    this.getProducts();
+                 })
+                 .catch((error)=>{
+                     console.log(error);
+     
+                 }) 
+        }
+    },
+    mounted(){
+        //抓取dom 元素
+        productsDom  = new bootstrap.Modal(document.getElementById('productModal'));
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'));
+
+         //從cookie取得token
+        var token = document.cookie.replace(/(?:(?:^|.*;\s*)bigtoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        //把cookie放進header
+        axios.defaults.headers.common['Authorization'] = token;
+
+        //登入驗證
+        axios.post(`${this.url}/api/user/check`)
+            .then((res)=>{
+                this.getProducts()
+            })
+            .catch((error)=>{
+                //失敗傳送回login
+                document.location.href="login.html";
+            })
+        
+    }
+})
+
+app.mount('#app');
